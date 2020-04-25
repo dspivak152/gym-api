bbc125cfc6d2d8d2d40ff434eb290ada0c4e0964
Danny Spivak <dspivak152@gmail.com>
2020-04-26 00:14:42 +0300

    a commit

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -4,9 +4,19 @@ const WorkoutController = {
     async index(req, res) {
         //const workouts = await Workout.find({}).deepPopulate('workoutTypeId rounds exercises exerciseType');
         //const workouts = await Workout.find({}).populate('workoutTypeId').populate('rounds').populate('rounds.exercises').populate('rounds.exercises.exerciseType');
-         const workouts = await Workout.find()
-            .populate({ path: 'workoutTypeId' })
-            .populate({ path: 'rounds', populate: 'exercises exerciseType' })
+        const workouts = await Workout.find()
+            .populate({
+                path: 'workoutTypeId'
+            })
+            .populate({
+                path: 'rounds',
+                populate: [{
+                    path: 'exercises',
+                    populate: {
+                        path: 'exerciseType'
+                    }
+                }]
+            })
         res.send(workouts);
     },
     async store(req, res) {
